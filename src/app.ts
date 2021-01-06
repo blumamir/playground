e0f5d892adca43fab82b07fa13217fe4946096a1
Amir Blum <amir@aspecto.io>
2021-01-06 18:50:14 +0200

    with sqs

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
 require('@aspecto/opentelemetry')({
     // logger: console,
-    local: false,
+    local: true,
     env: 'amir-test-env'
 });
 import express from 'express';
 import * as bodyParser from 'body-parser';
 import axios from 'axios';
+const x = require('aws-sdk');
 
+const { Consumer } = require('sqs-consumer');
+
+
+const sqs = Consumer.create({
+    queueUrl: 'https://sqs.eu-west-1.amazonaws.com/731241200085/dev-amir',
+    handleMessage: async (message: any) => {
+        console.log(message);
+    }
+})
+
+sqs.on('error', (err: any) => {
+    console.error(err.message);
+});
+
+sqs.on('processing_error', (err: any) => {
+    console.error(err.message);
+})
+
+sqs.start();
 
 const port = 7070;
 const app = express()
