c65bf548a4d0943c2fb4dc4ca96471daf4fd5aa7
Amir Blum <amir@aspecto.io>
2021-06-23 15:41:16 +0300

    test ci report

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,10 @@
-require('@aspecto/opentelemetry')({
-    // logger: console,
-    local: false,
-    env: 'amir-test-env'
-});
+const instrument = require('@aspecto/opentelemetry');
+instrument({ci: true, aspectoAuth: 'efff5d31-b87b-4638-8fb4-e6253e8337f3'});
+
 import express from 'express';
-import * as bodyParser from 'body-parser';
-import axios from 'axios';
-const x = require('aws-sdk');
-
-const { Consumer } = require('sqs-consumer');
-
-const sqs = Consumer.create({
-    queueUrl: 'https://sqs.eu-west-1.amazonaws.com/731241200085/dev-amir',
-    handleMessage: async (message: any) => {
-        console.log(message);
-    }
-})
-
-sqs.on('error', (err: any) => {
-    console.error(err.message);
+const app = express();
+app.use('/ci-test-endpoint', (req, res, next) => {
+  res.json({success: 'this is text, not boolean'});
 });
 
-sqs.on('processing_error', (err: any) => {
-    console.error(err.message);
-})
-
-sqs.start();
-
-const port = 7070;
-const app = express()
-    .use(bodyParser.json({ limit: '10mb' }));
-
-app.get('/outgoing-example', async (req, res) => {
-    const res2 = await axios.get(`https://api.publicapis.org/random`);
-    res.send(res2.data).status(200);
-});
-
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-});
+app.listen(3456, () => console.log('listening on port 3456'));
