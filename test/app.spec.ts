111243696f586da271e7fbc3ee1fd0b8e30ce660
Amir Blum <amir@aspecto.io>
2021-06-23 17:26:23 +0300

    ci: run test in ci

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,5 +1,9 @@
+import axios from 'axios';
+
 describe("test from ci", () => {
-  it("basic test", () => {
+  it("basic test", async () => {
+      const res = await axios.get('http://localhost:3456/ci-test-endpoint');
+      console.log(res.data);
       console.log('basic test is run');
   });
 });
