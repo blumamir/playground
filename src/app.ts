ca314348796aa01f82f5e8d8e8c49f94c63bd271
Amir Blum <amir@aspecto.io>
2021-06-23 17:32:29 +0300

    debug: trace not sent to aspecto

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-const instrument = require('@aspecto/opentelemetry');
-instrument({ci: true, aspectoAuth: 'efff5d31-b87b-4638-8fb4-e6253e8337f3'});
-
-import express from 'express';
-const app = express();
-app.use('/ci-test-endpoint', (req, res, next) => {
-  res.json({success: 'this is text, not boolean'});
+const instrument = require("@aspecto/opentelemetry");
+instrument({
+  ci: true,
+  aspectoAuth: "efff5d31-b87b-4638-8fb4-e6253e8337f3",
+  exportBatchTimeout: 1,
 });
 
-app.listen(3456, () => console.log('listening on port 3456'));
+import express from "express";
+const app = express();
+app.use("/ci-test-endpoint", (req, res, next) => {
+  res.json({ success: "this is text, not boolean" });
+});
+
+app.listen(3456, () => console.log("listening on port 3456"));
